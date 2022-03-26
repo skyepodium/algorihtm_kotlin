5f8386cfbb4519006c59b62da926f975cca90a44
skyepodium <skyepodium@gmail.com>
2022-03-26 15:59:46 +0900

    leetcode - binarysearch - recursive

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,24 @@
 package main.kotlin
 
 class Main {
+    fun main(args: Array<String>) {
+        println("Hellow World");
+    }
 }
 
 class Solution {
     fun search(nums: IntArray, target: Int): Int {
-        var s = 0
-        var e: Int = nums.size - 1
 
-        while(s <= e) {
-            var mid: Int = s + (e - s) / 2
+        return binarySearch(nums, target, 0, nums.size - 1)
+    }
 
-            if(nums[mid] < target) s = mid + 1
-            else if(nums[mid] == target) return mid
-            else e = mid - 1
-        }
+    private fun binarySearch(nums: IntArray, target: Int, s: Int, e: Int): Int {
+        if(s > e) return -1
 
-        return -1;
+        val mid: Int = s + (e - s) / 2
+
+        return if(nums[mid] < target) binarySearch(nums, target, mid+1, e)
+        else if(nums[mid] == target) mid
+        else binarySearch(nums, target, s, mid - 1)
     }
 }
